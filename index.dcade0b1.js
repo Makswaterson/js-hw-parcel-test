f0d515f5adf393aa6e82e5446e942414bd083f17
Makswaterson <85754214+Makswaterson@users.noreply.github.com>
2022-12-14 23:01:56 +0000

    Deploying to gh-pages from @ Makswaterson/goit-js-hw-08@5df26b5b8a909e8b8ce60a5528c06ae8214996ee 🚀

--- index.dcade0b1.js
+++
@@ -1,2 +0,0 @@
-console.log("fetch"),console.log("Hello Max"),console.log("Hello Roman"),console.log("js 56"),console.log("easy");
-//# sourceMappingURL=index.dcade0b1.js.map
--- index.f5d79298.js
+++
@@ -1,2 +0,0 @@
-console.log("fetch"),console.log("Hello Max"),console.log("Hello Roman"),console.log("js 56"),console.log("easy");
-//# sourceMappingURL=index.f5d79298.js.map
