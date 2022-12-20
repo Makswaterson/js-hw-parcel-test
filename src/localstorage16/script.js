806e0c1b4cf88430dd79de38c8f40f775196bdfe
Maksym Lukianenko <maks.lukyan@gmail.com>
2022-12-20 22:05:53 +0200

    modified XO

--- src/localstorage16/script.js
+++ src/localstorage16/script.js
@@ -1,7 +1,10 @@
 const content = document.querySelector('.content');
-let player = 'X';
-let stepX = [];
-let stepO = [];
+const KEY_X = 'keyX';
+const KEY_O = 'keyO';
+const CURRENT_PLAYER = 'player';
+let player = localStorage.getItem(CURRENT_PLAYER) || 'X';
+let stepX = JSON.parse(localStorage.getItem(KEY_X)) || [];
+let stepO = JSON.parse(localStorage.getItem(KEY_O)) ?? [];
 const win = [
   [1, 2, 3],
   [4, 5, 6],
@@ -25,25 +28,34 @@ function onClick(evt) {
   // '' => !false => true
   if (!evt.target.textContent) {
     const id = Number(evt.target.dataset.id);
+    evt.target.textContent = player;
     if (player === 'X') {
       stepX.push(id);
+      localStorage.setItem(KEY_X, JSON.stringify(stepX));
       const isWinner = checkWinner(stepX);
       if (isWinner) {
         console.log(`${player} is Winner 😎`);
-        reset();
+        setTimeout(() => {
+          reset();
+        }, 500);
         return;
       }
     } else {
       stepO.push(id);
+      localStorage.setItem(KEY_O, JSON.stringify(stepO));
       const isWinner = checkWinner(stepO);
+      evt.target.textContent = player;
       if (isWinner) {
         console.log(`${player} is Winner 😎`);
-        reset();
+        setTimeout(() => {
+          reset();
+        }, 500);
         return;
       }
     }
-    evt.target.textContent = player;
+
     player = player === 'X' ? 'O' : 'X';
+    localStorage.setItem(CURRENT_PLAYER, player);
   }
 }
 
@@ -54,7 +66,23 @@ function checkWinner(arr) {
 
 function reset() {
   content.innerHTML = markup;
+  localStorage.clear();
   player = 'X';
   stepX = [];
   stepO = [];
 }
+
+(function () {
+  try {
+    [...content.children].forEach(item => {
+      const id = Number(item.dataset.id);
+      if (stepX.includes(id)) {
+        item.textContent = 'X';
+      } else if (stepO.includes(id)) {
+        item.textContent = 'O';
+      }
+    });
+  } catch (e) {
+    console.log('localStorage empty');
+  }
+})();
