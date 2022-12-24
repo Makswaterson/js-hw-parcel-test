47b1bb55f1a6b7648eb6304c5399f45f7cdc2189
Makswaterson <85754214+Makswaterson@users.noreply.github.com>
2022-12-24 11:05:23 +0000

    Deploying to gh-pages from @ Makswaterson/js-hw-parcel-test@c9892a363bcb8d61f7183d5315035bed410b8d94 🚀

---
+++ index.d7ca28bf.js
@@ -0,0 +1,2 @@
+const n=["січень","лютий","","","",""];console.log(function(o){const t=new Date(o).getMonth();return n[t]}("10/11/2021")),clg;
+//# sourceMappingURL=index.d7ca28bf.js.map
---
+++ index.fd38d52a.js
@@ -0,0 +1,2 @@
+!function(){var n,o=["січень","лютий","","","",""];console.log((n=new Date("10/11/2021").getMonth(),o[n])),clg}();
+//# sourceMappingURL=index.fd38d52a.js.map
